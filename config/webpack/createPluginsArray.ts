f1966efe5954917d78296c01c4743b38e6d518a8
Julian Tellez <juliantellezmendez@gmail.com>
2019-05-09 23:07:07 +0100

    fmt: add prettier

--- config/webpack/createPluginsArray.ts
+++ config/webpack/createPluginsArray.ts
@@ -1,40 +1,36 @@
-import * as CopyWebpackPlugin       from 'copy-webpack-plugin';
-import * as HtmlWebpackPlugin       from 'html-webpack-plugin';
-import * as path                    from 'path';
-import {BundleAnalyzerPlugin}       from 'webpack-bundle-analyzer';
+import * as CopyWebpackPlugin from "copy-webpack-plugin";
+import * as HtmlWebpackPlugin from "html-webpack-plugin";
+import * as path from "path";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
-import {
-    Plugin,
-    EnvironmentPlugin
-} from 'webpack';
-import EnvironmentArgs from './EnvironmentArgs';
+import { Plugin, EnvironmentPlugin } from "webpack";
+import EnvironmentArgs from "./EnvironmentArgs";
 
-const PATH_ROOT = path.resolve(__dirname, '..', '..');
+const PATH_ROOT = path.resolve(__dirname, "..", "..");
 
 const createWebpackPluginsArray = (envArgs: EnvironmentArgs): Plugin[] => {
-    const plugins = [
-        new HtmlWebpackPlugin({
-            template: path.join(PATH_ROOT, 'assets', 'index.html')
-        }),
-        // new CopyWebpackPlugin([{
-        //     from: path.join(PATH_ROOT, 'assets', 'fonts'),
-        //     to: path.join(PATH_ROOT, 'dist')
-        // }]),
-        new EnvironmentPlugin({
-            NODE_ENV: envArgs.NODE_ENV
-        }),
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: path.join(PATH_ROOT, "assets", "index.html")
+    }),
+    // new CopyWebpackPlugin([{
+    //     from: path.join(PATH_ROOT, 'assets', 'fonts'),
+    //     to: path.join(PATH_ROOT, 'dist')
+    // }]),
+    new EnvironmentPlugin({
+      NODE_ENV: envArgs.NODE_ENV
+    })
+  ];
 
-    ];
+  if (envArgs.analyze) {
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: true
+      })
+    );
+  }
 
-    if (envArgs.analyze) {
-        plugins.push(
-            new BundleAnalyzerPlugin({
-                openAnalyzer: true
-            })
-        );
-    }
-
-    return plugins;
+  return plugins;
 };
 
 export default createWebpackPluginsArray;
