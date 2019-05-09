277eae1f961f666ae4caa44f12827f829e635214
Julian Tellez <juliantellezmendez@gmail.com>
2019-05-09 23:02:51 +0100

    scaffolding: webpack config

---
+++ config/webpack/createWebpackConfig.ts
@@ -0,0 +1,33 @@
+import * as path from 'path';
+import {Configuration} from 'webpack';
+import typescriptRule from './rules/typescript';
+import createWebpackPluginsArray from './createPluginsArray';
+import EnvironmentArgs from './EnvironmentArgs';
+
+const PATH_SRC = path.resolve(__dirname, '..', '..', 'src', 'main.tsx');
+const PATH_BUILD = path.resolve(__dirname, '..', '..', 'dist');
+
+const createWebpackConfig = (envArgs: any): Configuration => {
+    const environment = new EnvironmentArgs(envArgs)
+
+    return {
+        mode: environment.NODE_ENV,
+        entry: {
+            main: PATH_SRC,
+        },
+        output: {
+            filename: '[name].js',
+            path: PATH_BUILD,
+        },
+        devtool: 'source-map',
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js']
+        },
+        module: {
+            rules: [typescriptRule]
+        },
+        plugins: createWebpackPluginsArray(environment),
+    }
+}
+
+export default createWebpackConfig
---
+++ config/webpack/rules/css.ts
@@ -0,0 +1,21 @@
+import {Rule} from 'webpack';
+
+const cssRule: Rule = {
+    test: /\.css$/,
+    use: [
+        'style-loader',
+        {
+            loader: 'css-loader',
+            options: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: '[name]___[local]___[hash:base64:5]',
+                camelCase: true,
+                url: true,
+                import: true
+            }
+        },
+    ]
+};
+
+export default cssRule;
