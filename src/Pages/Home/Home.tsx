5c8da523b9f050c0271e92592b4cccd4f53ca59c
Julian Tellez <juliantellezmendez@gmail.com>
2019-08-29 22:54:50 +0100

    task: add home page

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+
+const Home = () => {
+    return (
+        <React.Fragment>
+            <Header />
+            <Footer />
+        </React.Fragment>
+    )
+}
+
+export default Home
