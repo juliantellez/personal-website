97cda6a799683936b961e7ba5955175c2724d301
Julian Tellez <juliantellezmendez@gmail.com>
2019-08-30 02:42:19 +0100

    task: add content to home page

--- src/Components/Root/Root.tsx
+++ src/Components/Root/Root.tsx
@@ -1,14 +1,32 @@
 import * as React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router';
 
 import Home from '../../Pages/Home/Home';
 import '../../Shared/Styles/global.css';
 
 import * as styles from './root.css';
+import ConwaysGameOfLife from '../ConwaysGameOfLife/ConwaysGameGameOfLife';
+import RoutePath from './Constants/RoutePath';
+
+
+const AppRoutes = () => (
+    <div className={styles.main}>
+        <Switch>
+            <Route path={RoutePath.GAME_OF_LIFE}>
+                <ConwaysGameOfLife />
+            </Route>
+            <Route>
+                <Home />
+            </Route>
+        </Switch>
+    </div>
+)
 
 const Root = () => (
-    <div className={styles.main}>
-        <Home />
-    </div>
+    <Router>
+        <AppRoutes />
+    </Router>
 );
 
 export default Root;
