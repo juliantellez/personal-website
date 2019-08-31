6ca42d5867b3e62b3f2c35dcaa4ce0ff8ec7c4a2
Julian Tellez <4896851+juliantellez@users.noreply.github.com>
2019-08-31 22:53:13 +0100

    Task/home page content (#3)
    * task: add contents to the homepage
    
    * task: add content to home page
    
    * chore: lint + fmt
    
    * task: ammending text overflow
    
    * chore: lint + fmt

---
+++ src/Components/Root/Constants/RoutePath.ts
@@ -0,0 +1,6 @@
+enum RoutePath {
+    GAME_OF_LIFE = '/game-of-life',
+    HOME = '/'
+}
+
+export default RoutePath;
--- src/Components/Root/Root.tsx
+++ src/Components/Root/Root.tsx
@@ -1,14 +1,31 @@
 import * as React from 'react';
+import {Route, Switch} from 'react-router';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import Home from '../../Pages/Home/Home';
 import '../../Shared/Styles/global.css';
+import ConwaysGameOfLife from '../ConwaysGameOfLife/ConwaysGameGameOfLife';
 
+import RoutePath from './Constants/RoutePath';
 import * as styles from './root.css';
 
-const Root = () => (
+const AppRoutes = () => (
     <div className={styles.main}>
-        <Home />
+        <Switch>
+            <Route path={RoutePath.GAME_OF_LIFE}>
+                <ConwaysGameOfLife />
+            </Route>
+            <Route>
+                <Home />
+            </Route>
+        </Switch>
     </div>
+);
+
+const Root = () => (
+    <Router>
+        <AppRoutes />
+    </Router>
 );
 
 export default Root;
