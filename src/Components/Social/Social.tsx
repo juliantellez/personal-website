d936a2f3d291cd687b60cb8bcbc1bc095ea10e79
Julian Tellez <4896851+juliantellez@users.noreply.github.com>
2019-09-01 16:25:43 +0100

    Task/add contact button (#7)
    * added: contact button
    
    * changed: fmt + lint

--- src/Components/Social/Social.tsx
+++ src/Components/Social/Social.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Github from '../Icons/Github';
 import Instagram from '../Icons/Instagram';
 import LinkedIn from '../Icons/LinkedIn';
+import Mail from '../Icons/Mail';
 import Twitter from '../Icons/Twitter';
 
 import * as styles from './social.css';
@@ -11,7 +12,9 @@ const urls = {
     twitter: 'https://twitter.com/_juliantellez',
     github: 'https://github.com/juliantellez',
     linkedin: 'https://www.linkedin.com/in/juliantellez',
-    instagram: 'https://www.instagram.com/_juliantellez'
+    instagram: 'https://www.instagram.com/_juliantellez',
+    mail:
+        'mailto:juliantellezmendez@gmail.com?subject=[ Source: juliantellez.com ]'
 };
 
 const Social = () => {
@@ -35,6 +38,11 @@ const Social = () => {
             <li className={styles.listElement}>
                 <a href={urls.github} target="_blank">
                     <Github />
+                </a>
+            </li>
+            <li className={styles.listElement}>
+                <a href={urls.mail}>
+                    <Mail />
                 </a>
             </li>
         </ul>
