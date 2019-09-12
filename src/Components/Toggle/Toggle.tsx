6a7c094a0bd7df0dce2675f7b5d2bd3ed90e3f57
Julian Tellez <juliantellezmendez@gmail.com>
2019-09-12 01:45:35 +0100

    changed: renaming colours

--- src/Components/Toggle/Toggle.tsx
+++ src/Components/Toggle/Toggle.tsx
@@ -2,24 +2,40 @@ import * as React from 'react';
 
 import * as styles from './toggle.scss';
 
-const Toggle = () => {
-    const onClick = () => {
-        const currentTheme = document.body.getAttribute('data-theme');
+class Toggle extends React.Component {
+    node = document.documentElement;
+
+    state = {
+        checked: true
+    }
+
+    componentWillMount() {
+        const currentTheme = this.node.getAttribute('data-theme');
+        this.setState({
+            checked: currentTheme === 'dark' ? false : true,
+        })
+    }
+
+    onClick = () => {
+        const currentTheme = this.node.getAttribute('data-theme');
         const nextTheme = currentTheme === 'dark' ? 'light' : 'dark';
-        document.body.setAttribute('data-theme', nextTheme);
+        this.node.setAttribute('data-theme', nextTheme);
     };
 
-    return (
-        <div>
-            <input
-                type="checkbox"
-                id="toggle"
-                className={styles.checkbox}
-                onClick={onClick}
-            />
-            <label htmlFor="toggle" className={styles.label} />
-        </div>
-    );
-};
+    render() {
+        return (
+            <div>
+                <input
+                    type="checkbox"
+                    id="toggle"
+                    defaultChecked={this.state.checked}
+                    className={styles.checkbox}
+                    onClick={this.onClick}
+                />
+                <label htmlFor="toggle" className={styles.label} />
+            </div>
+        );
+    }
+}
 
 export default Toggle;
