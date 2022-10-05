26d3b1c47891095a4b85d747e9089a8c20641b9b
Julian <juliantellezmendez@gmail.com>
2022-10-05 19:49:17 +0100

    chore(web): add blogs

--- packages/web/src/Api/github/getUser.ts
+++ packages/web/src/Api/github/getUser.ts
@@ -1,10 +1,11 @@
+import { logger } from '../../Middleware/logger'
 import { config } from '../config'
 
 import { getRawContent } from './getRawContent'
 
 const getUser = () => {
     return getRawContent<string>(config.getUser).then((response) => {
-        console.log({ response })
+        logger.log({ response })
     })
 }
 
