95458760efebd0f4cb54a1520b04a377355efd89
Julian <juliantellezmendez@gmail.com>
2022-10-06 02:47:55 +0100

    chore(web): home page

--- packages/web/src/Api/github/getUser.ts
+++ packages/web/src/Api/github/getUser.ts
@@ -1,11 +1,19 @@
-import { logger } from '../../Middleware/logger'
 import { config } from '../config'
 
 import { getRawContent } from './getRawContent'
 
-const getUser = () => {
-    return getRawContent<string>(config.getUser).then((response) => {
-        logger.log({ response })
+interface UserResponse {
+    avatar_url: string
+}
+
+export interface User {
+    avatarUrl: string
+}
+const getUser = (): Promise<User> => {
+    return getRawContent<UserResponse>(config.getUser).then((response) => {
+        return {
+            avatarUrl: response.avatar_url,
+        }
     })
 }
 
