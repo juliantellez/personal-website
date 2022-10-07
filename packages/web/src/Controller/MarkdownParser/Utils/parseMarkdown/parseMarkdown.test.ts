9b4a0f0619dbf593d662f2983778175bc11b2b9c
Julian Tellez <4896851+juliantellez@users.noreply.github.com>
2022-10-07 04:56:50 +0100

    chore(web): blog code highlight (#40)
    * chore(web): blogs description
    
    * chore(web): blog styles
    
    * test: update

--- packages/web/src/Controller/MarkdownParser/Utils/parseMarkdown/parseMarkdown.test.ts
+++ packages/web/src/Controller/MarkdownParser/Utils/parseMarkdown/parseMarkdown.test.ts
@@ -8,23 +8,28 @@ describe('parseMarkdown', () => {
         const actual = '# Foo Baz'
 
         const expected = '<h1>Foo Baz</h1>\n'
-
-        expect(parseMarkdown(actual)).toBe(expected)
+        const value = parseMarkdown(actual)
+        expect(value).toBe(expected)
     })
 
     it('should parse code blocks to hml strings', () => {
         const filePath = path.resolve(__dirname, './codeBlock.md')
         const actual = fs.readFileSync(filePath, { encoding: 'utf-8' })
 
-        const expected = `<pre><code class="language-javascript">import foo from './foo'\nexport default foo\n</code></pre>\n`
+        const expected =
+            '<pre><code class="language-javascript hljs"><span class="hljs-keyword">import</span> foo <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;./foo&#x27;</span>\n' +
+            '<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> foo\n' +
+            '</code></pre>\n'
 
-        expect(parseMarkdown(actual)).toBe(expected)
+        const value = parseMarkdown(actual)
+
+        expect(value).toBe(expected)
     })
 
     it('should handle no inputs', () => {
         const actual = void 0 as any
         const expected = ''
-
-        expect(parseMarkdown(actual)).toBe(expected)
+        const value = parseMarkdown(actual)
+        expect(value).toBe(expected)
     })
 })
